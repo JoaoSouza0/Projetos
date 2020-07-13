f9613483cb87f942490f1ddf179f63dda84e3a4b
JoaoSouza0 <jooavictorcostasouza15@gmail.com>
2020-07-13 12:17:29 -0300

    "Otimizando Script para previnir erros"

--- Projetos Cursos/Calculadora/src/script.js
+++ Projetos Cursos/Calculadora/src/script.js
@@ -1,18 +1,19 @@
 
+var contador = 1
 function getNumber(tipo, valor) {
 
     if (valor == 'c') {
         document.getElementById('tela').value = ''
     }
 
-    if (tipo == 'acao') {
+    if (tipo == 'acao' && contador == 1) {
         if (valor === '-' || valor === '/' || valor === '*' || valor === '+' || valor === '.') {
 
             document.getElementById('tela').value += valor
-        } else if ( valor === '=' )
-        {   
+            contador = 0
+        } else if (valor === '=') {
             var tela = eval(document.getElementById('tela').value)
-           
+
             document.getElementById('tela').value = tela
         }
 
@@ -20,6 +21,6 @@ function getNumber(tipo, valor) {
 
         document.getElementById('tela').value += valor
 
-
+        contador = 1
     }
 }
